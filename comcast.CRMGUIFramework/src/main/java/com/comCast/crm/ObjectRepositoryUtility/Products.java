22edf4d6a1fee4aec98918a0f34bcdfe96c08f9b
tezsingh888 <112232367+tezsingh888@users.noreply.github.com>
2025-06-25 12:57:12 +0530

    Products,Added new Pom class

---
+++ comcast.CRMGUIFramework/src/main/java/com/comCast/crm/ObjectRepositoryUtility/Products.java
@@ -0,0 +1,9 @@
+package com.comCast.crm.ObjectRepositoryUtility;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Products {
+@FindBy(xpath="//input[@alt='Create Product. . .']")
+private WebElement createProductImgBtn;
+}
